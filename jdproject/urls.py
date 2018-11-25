88e54f02d049332ff64132bac85dd09c5e26d61a
Unknown <jdibicho@gmail.com>
2018-11-25 00:02:51 -0600

    new commit

---
+++ jdproject/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('productos.urls')),
+    path('productos/', include('productos.urls')),
+    path('admin/', admin.site.urls)
+]
---
+++ productos/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('Productos/', views.index, name='index'),
+    path('editar/<int:id>', views.editar, name='edit'),
+    path('eliminar/<int:id>', views.eliminar, name='eliminar'),
+    path('crear/', views.crear, name='crear'),
+    path('categorias/', views.cats, name="categorias"),
+    path('borrar/<int:id>', views.catdelete, name='catdelete'),
+    path('crearCategoria/', views.crearcat, name='crearcat'),
+    path('editarCategoria/<int:id>', views.catedit, name='catedit'),
+    path('comprar/<int:id>', views.comprar, name="comprar"),
+    path('error/', views.error, name="error")
+
+#      path('', views.index, name='index'),
+#     path('productos/', views.index, name='index'),
+#     path('edit/<int:id>', views.edit, name='edit'),
+#     path('eliminar/<int:id>', views.eliminar, name='eliminar'),
+#     path('crear/', views.crear, name='crear'),
+#     path('categorias/', views.cats, name="categorias"),
+#     path('catdelete/<int:id>', views.catdelete, name='catdelete'),
+#     path('crearcat/', views.crearcat, name='crearcat'),
+#     path('catedit/<int:id>', views.catedit, name='catedit'),
+#     path('comprar/<int:id>', views.comprar, name="comprar"),
+#     path('error/', views.error, name="error")
+ ]
